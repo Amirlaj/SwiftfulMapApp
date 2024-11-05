85181d1238dce4747eeed63aecc0ffb46241897c
Amir Lajevardi <amir.lajevardi@city.ac.uk>
2024-11-05 13:17:58 +0000

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Amir Lajevardi on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
