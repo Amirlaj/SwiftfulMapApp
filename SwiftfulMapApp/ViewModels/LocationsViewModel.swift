7a6a557d4890d4a3ae0e861a39ac595853770cce
Amir Lajevardi <amir.lajevardi@city.ac.uk>
2024-12-26 11:27:05 +0000

    session fixed upto third

---
+++ SwiftfulMapApp/ViewModels/LocationsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationsViewModel.swift
+//  SwiftfulMapApp
+//
+//  Created by Amir Lajevardi on 25/12/2024.
+//
+
+import Foundation
---
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  SwiftfulMapApp
+//
+//  Created by Amir Lajevardi on 25/12/2024.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
